a1dde542d7003b92aefabb0500ffcb60b36afc5e
Martin <trochezmartin@gmail.com>
2023-09-27 16:45:36 -0300

    asd

---
+++ script/carousel.js
@@ -0,0 +1,18 @@
+let sliderIndex = [1, 1]
+let sliderId = ["slider-imagenes", "slider-noticia"]
+mostrarSlide(1, 0);
+mostrarSlide(1, 1);
+
+function mostrarSlide(n, no) {
+	let x = document.getElementsByClassName(sliderId[no]);
+	if (n > x.length) { sliderIndex[no] = 1 }
+	if (n < 1) { sliderIndex[no] = x.length }
+	for (let i = 0; i < x.length; i++) {
+		x[i].style.display = "none";
+	}
+	x[sliderIndex[no] - 1].style.display = "block";
+}
+
+function cambiarSlide(n, no) {
+	mostrarSlide(sliderIndex[no] += n, no);
+}
